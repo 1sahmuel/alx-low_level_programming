0cde542abfe6de947c87e0a342608ab0076e429a
1sahmuel <1sahmuel@gmail.com>
2022-11-01 02:47:06 -0700

    Task 0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,30 @@
 #include "main.h"
 /**
- * read_textfile - reads a textfile
- * @filename: file to read
- * @letters: amount of bytes to read
- * Return: printed letters count
+ * create_file - creates a file
+ * @filename: filename to grab
+ * @text_content: content to add
+ * Return: int value
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+int create_file(const char *filename, char *text_content)
 {
-	int fd, printed, wrote;
-	char *buff;
+	int fd, len = 0;
+	long int wrote;
 
-	buff = malloc(letters);
-	if (buff == NULL)
-		return (0);
 	if (filename == NULL)
-		return (0);
-	fd = open(filename, O_RDWR);
+		return (-1);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
-		return (0);
-	printed = read(fd, buff, letters);
-	if (printed == -1)
-		return (0);
-	wrote = write(STDOUT_FILENO, buff, printed);
-	if (wrote == -1)
-		return (0);
+		return (-1);
+	if (text_content != NULL)
+	{
+
+		while (text_content[len])
+			len++;
+		wrote = write(fd, text_content, len);
+		if (wrote == -1)
+			return (-1);
+	}
 	if (close(fd) == -1)
-		return (0);
-	free(buff);
-	return (printed);
+		return (-1);
+	return (1);
 }
